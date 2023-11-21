a70062c51acfbcd59f55fcae04466fb804f11cf3
BERKABZ <150333064+BERKABZ@users.noreply.github.com>
2023-11-22 05:34:47 +0800

    Add files via upload

---
+++ En4t/src/main/java/com/mycompany/en4t/En4t.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.en4t;
+
+/**
+ *
+ * @author USER
+ */
+public class En4t {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
